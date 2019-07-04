7956f2a4ade42d9ac9dea713598fcb4889d92b27
Cinthia Nava <cinthia.nava@rhinosystems.mx>
2019-07-04 15:56:33 -0500

    Commit se crea proyecto para registrar grupos usando plantilla de boostwatch

---
+++ altagrupos/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mt-2 text-center">Alta de grupos</h1>
+    </div>
+  );
+}
+
+export default App;
